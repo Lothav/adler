dc88c0d7bd4c5475112518cce6b1f7b0652b2c04
Luiz0tavio <luizotrv@gmail.com>
2016-11-16 21:52:18 -0200

    commit.sh | Gulpfile

---
+++ Gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var pump = require('pump');
+var concat = require('gulp-concat');
+
+gulp.task('concat_uglify', function(cb) {
+    gulp.src([
+        'js/phaser.js',
+        'js/create.js',
+        'js/preload.js',
+        'js/update.js',
+        'js/globals.js'
+    ]).pipe(concat('adler.js'))
+        .pipe(gulp.dest('./'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./'));
+    cb();
+});
+
+gulp.task('default', ['concat_uglify']);
---
+++ commit.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+gulp &&
+git commit -am "$1" && git push origin master &&
+mv adler.js ../LuizOtav.io/adler &&
+cp -r assets ../LuizOtav.io/adler &&
+cd ../LuizOtav.io &&
+git commit -am "changes in @Luiz0tavio/adler -> $1" && git push origin master &&
+cd ../adler
