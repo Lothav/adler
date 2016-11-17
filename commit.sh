ea8b7ca2759d35c8ebb4fb6b1d7e91250701f400
Luiz0tavio <luizotrv@gmail.com>
2016-11-16 22:37:21 -0200

    devil sprites

--- commit.sh
+++ commit.sh
@@ -5,5 +5,5 @@ git commit -am "$1" && git push origin master &&
 mv adler.js ../LuizOtav.io/adler &&
 cp -r assets ../LuizOtav.io/adler &&
 cd ../LuizOtav.io &&
-git commit -am "changes in @Luiz0tavio/adler -> $1" && git push origin master &&
+git add * && git commit -am "changes in @Luiz0tavio/adler -> $1" && git push origin master &&
 cd ../adler
